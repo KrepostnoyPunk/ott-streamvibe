29c37ac2ebf0a39b7d06fd8ffc851d0bd3b2eb45
FREEMAN-LT\stepa <stepansidorov3234@gmail.com>
2025-02-13 21:57:00 +0300

    Исправил верстку слайдера, теперь больше похоже на слайдер и выглядит логичнее. Поковырялся в переключении слайдов, но к норм варианту пока не пришел.

--- scripts/Slider.js
+++ scripts/Slider.js
@@ -4,8 +4,8 @@ class Slider{
         sliderControls: '[data-js-slider-controls]',
         sliderControlsBtn: '[data-js-slider-controls-btn]',
         pageMark: '[data-js-slider-page-mark]',
-        slidesList: '[data-js-slider-slides-list]',
-        slideItem: '[data-js-slider-item]',
+        slidesWrapper: '[data-js-slider-slides-wrapper]',
+        slide: '[data-js-slider-slide]',
         mobileProgressBar: '[data-js-slider-mobile-progress-bar]',
     }
 
@@ -23,23 +23,33 @@ class Slider{
         this.sliderControlsEl = document.querySelector(this.selectors.sliderControls);
         this.sliderControlsBtnEls = this.sliderControlsEl.querySelectorAll(this.selectors.sliderControlsBtn);
         this.pageMarkEls = this.sliderControlsEl.querySelectorAll(this.selectors.pageMark);
-        this.slidesListEl = this.rootEl.querySelector(this.selectors.slidesList);
-        this.slideItemEls = this.rootEl.querySelector(this.selectors.slideItem);
+        this.slidesWrapperEl = this.rootEl.querySelector(this.selectors.slidesWrapper);
+        this.slideItemEls = this.rootEl.querySelectorAll(this.selectors.slide);
         this.mobileProgressBarEl = this.rootEl.querySelector(this.selectors.mobileProgressBar)
 
-        this.currentSlide = 0;
+        this.totalSlides = this.slideItemEls.length; // 4 but max === 3, b-se 0 === 1 by array rules
+        this.currentSlide = 0; 
 
         this.bindEvents()
     }
 
     UpdateSlide = (event) => {
-        if(event.target.id === "prev-btn"){
-
-        } else if(event.target.id === "next-btn"){
-            
-        } else {
-            return
+        if(this.currentSlide >= this.totalSlides){ // if currentSlide greater then totalSlides quantity
+            this.currentSlide = 0; // drop currentSlide to start
+        } else if (this.currentSlide < 0){
+            this.currentSlide = this.totalSlides - 1; // if currentSlide less then 0 let currentSlide be equal to last slide
         }
+
+        // TODOOOO
+        if(event.target.id === "prev-btn"){
+            this.currentSlide--;
+            this.slidesWrapperEl.style.transform = `translateX(+${this.slideItemEls[0].offsetWidth}px)`;
+        } else {
+            this.currentSlide++;
+            this.slidesWrapperEl.style.transform = `translateX(-${this.slideItemEls[0].offsetWidth}px)`;
+        }
+
+        console.log(this.currentSlide);
     }
     
     bindEvents(){
